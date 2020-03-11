553f999b74b2c96dcfbc741cd85556c060850adf
Austin Lee <bretonspellsword@gmail.com>
2020-03-11 17:03:27 -0500

    Register and Login page displays; navigation still not functional

--- cs3530Project/client/main.js
+++ cs3530Project/client/main.js
@@ -2,11 +2,21 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './registrationNlogin.html';
+
+Router.configure({
+	layoutTemplate: 'main'
+});
+
+Router.configure({
+	layoutTemplate: 'registerNloginpage'
+});
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
+
 
 Template.hello.helpers({
   counter() {
@@ -14,9 +24,14 @@ Template.hello.helpers({
   },
 });
 
+
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Router.route('/main/',function(){this.render('main');});
+Router.route('/registerNloginpage/',function(){this.render('registerNloginpage');});
+
