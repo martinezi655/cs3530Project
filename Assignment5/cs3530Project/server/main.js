18e19992392055058d64d404cc5e00570fca337b
CMelhaire <bretonspellsword@gmail.com>
2020-05-11 19:06:28 -0500

    Update main.js

--- Assignment5/cs3530Project/server/main.js
+++ Assignment5/cs3530Project/server/main.js
@@ -4,13 +4,16 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
-insert_collectionStudents: function(StudentId, StudentName, StudentAddress, StudentDOB) 
+ function insert_collectionStudents(StudentId, StudentName, StudentAddress, StudentDOB) 
 {
 	console.log("in insert_collectionStudents");
 	var MongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://localhost:3000";
-	MongoClient.connect(url, function(err,db) 
-	{
+	var url = "mongodb://127.0.0.1:27017/myOnlineEducationDatabase";
+	MongoClient.connect(
+	url, function(err,db) 
+
+	{ 
+	console.log("Connected to Server!");
 		if (err) throw err;
 		var myobj = 
 		{
@@ -20,11 +23,14 @@ insert_collectionStudents: function(StudentId, StudentName, StudentAddress, Stud
 			"Date_of_Birth": StudentDOB
 		}
 		var dbo = db.db("myOnlineEducationDatabase") //use database name here
-		dbo.collection("Student").insertOne(myobj, function(err,res) 
+		dbo.Student.insertOne(
+		myobj.toString(), function(err,res) 
 		{
 			if (err) throw err;
 			Console.log("1 record inserted");
 			db.close();
-		});
-	});
+		}
+		);
+	}
+	);
 }
